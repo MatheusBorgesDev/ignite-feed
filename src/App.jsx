4b6cbe881a8725a0147204619a35029c8134eba3
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-22 14:52:07 -0300

    feat: :sparkles: add Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { Post } from "./Post";
 import { Header } from "./components/Header";
 
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Matheus Borges"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque libero doloremque nostrum enim voluptas blanditiis cupiditate odio, est ratione molestiae. Odio ratione dicta corporis voluptate nam voluptas iusto sapiente!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Matheus Borges"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque libero doloremque nostrum enim voluptas blanditiis cupiditate odio, est ratione molestiae. Odio ratione dicta corporis voluptate nam voluptas iusto sapiente!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
