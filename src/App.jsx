5fbbff98556e7251f6260debb44557905a5f54f3
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-22 07:44:35 -0300

    feat: :sparkles: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import "./global.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Matheus Borges"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla neque libero doloremque nostrum enim voluptas blanditiis cupiditate odio, est ratione molestiae. Odio ratione dicta corporis voluptate nam voluptas iusto sapiente!"
