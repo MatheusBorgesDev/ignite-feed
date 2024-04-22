183101781fb08e741037b1c11d6b3b32491114c6
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-22 16:11:53 -0300

    feat: :sparkles: add Post component

--- src/Post.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-export function Post(props) {
-  return (
-    <div>
-      <h1>{props.author}</h1>
-      <p>{props.content}</p>
-    </div>
-  );
-}
---
+++ src/components/Post.jsx
@@ -0,0 +1,54 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/matheusborgesDev.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Matheus Borges</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+
+        <time title="22 de abril às 15h10" dateTime="2024-04-22 03:10:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat.
+        </p>
+
+        <p>O nome do projeto é DoctorCare 🚀 </p>
+
+        <p>
+          <a href="#">matheus.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
