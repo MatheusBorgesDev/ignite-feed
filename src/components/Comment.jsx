95e50821e1c3c99c03d32fa8bd5360ab6c536802
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-22 17:11:23 -0300

    feat: :sparkles: add Comment component to Post component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/matheusborgesDev.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Matheus Borges</strong>
+
+              <time title="22 de abril às 15h10" dateTime="2024-04-22 03:10:00">
+                Há cerca de 1h
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
