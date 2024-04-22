bea7f18b187a3ffca621d3a924b3c443d5a12617
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-22 17:33:07 -0300

    feat: :sparkles: add Avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,15 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/matheusborgesDev.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/matheusborgesDev.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from "@phosphor-icons/react";
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/matheusborgesDev.png"
-        />
+        <Avatar src="https://github.com/matheusborgesDev.png" />
 
         <strong>Matheus Borges</strong>
 
